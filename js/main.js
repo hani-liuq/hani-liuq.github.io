96b93586944bbc41db2ff2f97ddb93d7e40be740
liuqiang <916000612@qq.com>
2019-09-09 22:12:33 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 const apiServer = 'https://hani-liuq.github.io'
+// const apiServer = 'http://118.25.114.172'
+// const apiServer = 'http://127.0.0.1:8080'
 const root = document.getElementById('mainJS').getAttribute('root');
 document.write(`<script type="text/javascript" src="${root}plugins/layui/layui.js"></script>`)
 document.write(`<script type="text/javascript" src="${root}js/vue.min.js"></script>`)
 document.write(`<script type="text/javascript" src="${root}js/axios.min.js"></script>`)
 document.write(`<script type="text/javascript" src="${root}js/http.js"></script>`)
 
-
-function setIframeHeight(iframe) {
-	if (iframe) {
-		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-		if (iframeWin.document.body) {
-			iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-		}
-	}
-};
